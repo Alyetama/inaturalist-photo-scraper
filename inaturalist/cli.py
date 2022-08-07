a644f6dcc8cfa5ed4845a29dd692a3eb4287f7c5
Alyetama <56323389+Alyetama@users.noreply.github.com>
2022-08-06 21:38:29 -0400

    Solve an inat API issue for when number of results > 10,000

--- inaturalist/cli.py
+++ inaturalist/cli.py
@@ -4,7 +4,6 @@
 import argparse
 
 from dotenv import load_dotenv
-
 from inaturalist.scraper import InaturalistPhotoScraper
 
 
@@ -25,7 +24,7 @@ def _opts() -> argparse.Namespace:
                         help='Page to resume from',
                         type=int,
                         default=0)
-    parser.add_argument('-e',
+    parser.add_argument('-P',
                         '--stop-at-page',
                         help='Page to stop at',
                         type=int)
@@ -37,15 +36,31 @@ def _opts() -> argparse.Namespace:
     parser.add_argument('--upload-to-s3',
                         help='Upload to a S3-compatible bucket',
                         action='store_true')
-    parser.add_argument('--one-page-only',
+    parser.add_argument('-O',
+                        '--one-page-only',
                         help='Terminate after completing a single page',
                         action='store_true')
+    parser.add_argument('-r',
+                        '--results-per-page',
+                        help='Number of results per page',
+                        type=int,
+                        default=200)
+    parser.add_argument('-s',
+                        '--start-year',
+                        help='Year to start from '
+                        '(only relevant when number of observations > 10,000)',
+                        type=int,
+                        default=2008)
+    parser.add_argument('-e',
+                        '--end-year',
+                        help='Year to stop at '
+                        '(only relevant when number of observations > 10,000)',
+                        type=int)
     return parser.parse_args()
 
 
 def main() -> None:
     """Main function."""
-    load_dotenv()
     args = _opts()
     scraper = InaturalistPhotoScraper(
         taxon_id=args.taxon_id,
@@ -54,9 +69,13 @@ def main() -> None:
         stop_at_page=args.stop_at_page,
         resume_from_uuid_index=args.resume_from_uuid_index,
         upload_to_s3=args.upload_to_s3,
-        one_page_only=args.one_page_only)
+        one_page_only=args.one_page_only,
+        results_per_page=args.results_per_page,
+        start_year=args.start_year,
+        end_year=args.end_year)
     scraper.run()
 
 
 if __name__ == '__main__':
+    load_dotenv()
     main()
--- inaturalist/scraper.py
+++ inaturalist/scraper.py
@@ -10,6 +10,7 @@ import signal
 import sys
 import time
 import urllib.parse
+from datetime import datetime
 from pathlib import Path
 from typing import Optional, Union
 
@@ -30,8 +31,11 @@ class InaturalistPhotoScraper:
                  resume_from_page: int = 0,
                  stop_at_page: Optional[int] = None,
                  resume_from_uuid_index: int = 0,
-                 upload_to_s3: bool = True,
-                 one_page_only: bool = False):
+                 upload_to_s3: bool = False,
+                 one_page_only: bool = False,
+                 results_per_page: int = 200,
+                 start_year: Optional[int] = 2008,
+                 end_year: Optional[int] = None):
         super(InaturalistPhotoScraper, self).__init__()
         self.taxon_id = taxon_id
         self.output_dir = output_dir
@@ -40,20 +44,24 @@ class InaturalistPhotoScraper:
         self.resume_from_uuid_index = resume_from_uuid_index
         self.upload_to_s3 = upload_to_s3
         self.one_page_only = one_page_only
-        self.logger = self._logger()
+        self.results_per_page = results_per_page
+        self.start_year = start_year
+        self.end_year = end_year
         self.s3 = self._s3_client()
+        self._logger = self._logger()
         self.data = {
             'uuids': [],
             'observations': [],
             'failed_observations': [],
             'failed_downloads': []
         }
+        self.is_large_results = False
 
     def _s3_client(self) -> Optional[Minio]:
         """Returns a Minio client instance.
 
         Returns:
-            Minio: Minio client instance.
+            Minio: Minio client instance if `upload_to_s3` is set to `True`.
         """
         s3_client = None
         if self.upload_to_s3:
@@ -89,16 +97,16 @@ class InaturalistPhotoScraper:
             sig (int): Signal number.
             _ (): Unused.
         """
-        self.logger.info(f'>>>>>>>>>> Latest page: {self.resume_from_page}')
-        self.logger.info(
+        logger.info(f'>>>>>>>>>> Latest page: {self.resume_from_page}')
+        logger.info(
             f'>>>>>>>>>> Latest UUID index: {self.resume_from_uuid_index}')
-        self.logger.warning(
+        if self.start_year:
+            logger.info(f'>>>>>>>>>> Latest year: {self.start_year}')
+        logger.warning(
             f'Failed observations: {self.data["failed_observations"]}')
-        self.logger.warning(
-            f'Failed downloads: {self.data["failed_downloads"]}')
-        self.logger.warning(
-            f'\nKeyboardInterrupt (id: {sig}) has been caught...')
-        self.logger.warning('Terminating the session gracefully...')
+        logger.warning(f'Failed downloads: {self.data["failed_downloads"]}')
+        logger.warning(f'\nKeyboardInterrupt (id: {sig}) has been caught...')
+        logger.warning('Terminating the session gracefully...')
         sys.exit(1)
 
     @staticmethod
@@ -118,7 +126,7 @@ class InaturalistPhotoScraper:
                      url: str,
                      params: Optional[dict] = None,
                      as_json: bool = True,
-                     **kwargs) -> Union[Response, dict, None]:
+                     **kwargs) -> Optional[Union[Response, dict]]:
         """Returns a GET request.
 
         Args:
@@ -136,7 +144,6 @@ class InaturalistPhotoScraper:
         if params:
             encoded_params = self._encode_params(params)
             url = f'{url}?{encoded_params}'
-
         try:
             r = requests.get(url, headers=headers, **kwargs)
             r.raise_for_status()
@@ -145,59 +152,75 @@ class InaturalistPhotoScraper:
             else:
                 return r
         except (HTTPError, JSONDecodeError) as e:
-            self.logger.error(f'Failed to get {url}! (ERROR: {e})')
-            self.logger.exception(e)
+            logger.error(f'Failed to get {url}! (ERROR: {e})')
+            logger.exception(e)
             self.data['failed_observations'].append(url)
             return
         finally:
             time.sleep(1)
 
-    def get_num_pages(self) -> int:
+    def get_num_pages(self, on_year: Optional[int] = None) -> tuple:
         """Returns the number of pages.
 
         Returns:
-            int: Number of pages.
+            tuple: A tuple of (number of pages, number of observations).
         """
         url = 'https://api.inaturalist.org/v2/observations'
         params = {'taxon_id': self.taxon_id}
+        if on_year:
+            params.update({'year': on_year})
         r = self._get_request(url, params=params)
         if not r:
             sys.exit('Failed to get number of pages!')
         total_results = r['total_results']
-        return total_results // 200 + 1
+        return total_results // self.results_per_page + 1, r['total_results']
 
-    def get_observations_uuids(self, page: int) -> Optional[list]:
+    def get_observations(
+        self,
+        page: int,
+        additional_params: Optional[dict] = None
+    ) -> Optional[Union[Response, dict]]:
         """Returns a list of observation UUIDs.
 
         Args:
             page (int): Page number.
+            additional_params (dict, optional): Additional parameters to send.
 
         Returns:
-            list: List of observation UUIDs, or `None` if the request failed.
+            Union[Response, dict, None]: Response object or observations data.
         """
         url = 'https://api.inaturalist.org/v2/observations'
         params = {
             'taxon_id': self.taxon_id,
             'photos': 'true',
             'page': page,
-            'per_page': 200,
+            'per_page': self.results_per_page,
             'order': 'asc',
-            'order_by': 'created_at',
-            'fields': 'uuid'
+            'order_by': 'observed_on',
+            'fields': 'uuid,observed_on',
         }
+        if additional_params:
+            params.update(additional_params)
 
-        resp_json = self._get_request(url, params=params)
-        if not resp_json:
+        observations = self._get_request(url, params=params)
+        if not observations:
+            logger.error('Could not find observations for this request!')
             return
-
-        uuids = [x['uuid'] for x in resp_json['results']]
-        return uuids
+        return observations
 
     def _put_object(self,
-                    bucket_name,
-                    object_name,
-                    data,
+                    bucket_name: str,
+                    object_name: str,
+                    data: bytes,
                     content_type: str = 'application/octet-stream'):
+        """Uploads an object to S3.
+
+        Args:
+            bucket_name (str): Bucket name.
+            object_name (str): Object name.
+            data (Union[bytes, str]): Data to upload.
+            content_type (str, optional): Content type.
+        """
         return self.s3.put_object(bucket_name,
                                   object_name,
                                   io.BytesIO(data),
@@ -215,8 +238,9 @@ class InaturalistPhotoScraper:
             bool: Whether the download was successful or not. None if the
                 request failed.
         """
-        url = f'https://www.inaturalist.org/observations/{observation_uuid}.json'  # noqa E501
-        self.logger.debug(f'({observation_uuid}) Requesting observation')
+        url = \
+            f'https://www.inaturalist.org/observations/{observation_uuid}.json'
+        logger.debug(f'({observation_uuid}) Requesting observation')
 
         observation = self._get_request(url, allow_redirects=True)
         if not observation:
@@ -226,13 +250,13 @@ class InaturalistPhotoScraper:
         self.data['observations'].append(observation)
         observation_photos = observation['observation_photos']
         if not observation_photos:
-            self.logger.debug(f'({observation_uuid}) No photos... Skipping...')
+            logger.debug(f'({observation_uuid}) No photos... Skipping...')
             return
 
         for photo in observation_photos:
             photo_url = photo['photo']['large_url']
             photo_uuid = photo['photo']['uuid']
-            self.logger.debug(f'({photo_uuid}) Downloading...')
+            logger.debug(f'({photo_uuid}) Downloading...')
 
             suffix = Path(photo_url).suffix.lower()
             if not suffix or suffix == '.jpeg':
@@ -258,14 +282,39 @@ class InaturalistPhotoScraper:
                     pass
 
                 try:
-                    self._put_object(os.environ['S3_BUCKET_NAME'], fname, data)
+                    self._put_object(os.environ['S3_BUCKET_NAME'], fname,
+                                     json.dumps(self.data).encode())
                 except InvalidResponseError:
                     self.data['failed_downloads'].append(photo)
             else:
                 with open(Path(f'{self.output_dir}/{fname}'), 'wb') as f:
                     f.write(r.content)
-            self.logger.debug(f'({photo_uuid}) ✅ Downloaded')
+            logger.debug(f'({photo_uuid}) ✅ Downloaded')
         return True
+
+    def _write_progress_file(self, fname: str, by_year: bool = False):
+        logger.info(
+            f'Writing progress file for {self.taxon_id} for the first '
+            'time...')
+        if by_year:
+            progress = {}
+            if not self.end_year:
+                self.end_year = datetime.now().year
+            year_range = range(self.start_year, self.end_year + 1)
+            for year in year_range:
+                num_pages, _ = self.get_num_pages(on_year=year)
+                progress_per_page = {
+                    str(k): 'pending'
+                    for k in range(num_pages + 1)
+                }
+                progress[year] = progress_per_page
+        else:
+            num_pages, _ = self.get_num_pages()
+            progress = {str(k): 'pending' for k in range(num_pages)}
+
+        encoded_json = json.dumps(progress).encode()
+        self._put_object(os.environ['S3_LOGS_BUCKET_NAME'], fname,
+                         encoded_json, 'application/json')
 
     def check_progress(self,
                        page: Union[int, str],
@@ -284,11 +333,16 @@ class InaturalistPhotoScraper:
         page = str(page)
         progress_fname = f'{self.taxon_id}_progress.json'
 
+        year = str(self.start_year) if self.is_large_results else None
+
         if mark_as_complete:
             progress_raw = self.s3.get_object(
                 os.environ['S3_LOGS_BUCKET_NAME'], progress_fname)
             progress = json.loads(progress_raw.read().decode())
-            progress[page] = 'complete'
+            if year:
+                progress[year][page] = 'complete'
+            else:
+                progress[page] = 'complete'
             encoded_json = json.dumps(progress).encode()
             self._put_object(os.environ['S3_LOGS_BUCKET_NAME'], progress_fname,
                              encoded_json, 'application/json')
@@ -299,23 +353,24 @@ class InaturalistPhotoScraper:
             x for x in logs_objects if x.object_name == progress_fname
         ]
         if not progress_exists:
-            logger.info(
-                f'Writing progress file for {self.taxon_id} for the first '
-                'time...')
-            num_pages = self.get_num_pages()
-            progress = {str(k): 'pending' for k in range(num_pages)}
-            encoded_json = json.dumps(progress).encode()
-            self._put_object(os.environ['S3_LOGS_BUCKET_NAME'], progress_fname,
-                             encoded_json, 'application/json')
+            if year:
+                self._write_progress_file(progress_fname, by_year=True)
+            else:
+                self._write_progress_file(progress_fname)
 
         progress_raw = self.s3.get_object(os.environ['S3_LOGS_BUCKET_NAME'],
                                           progress_fname)
         progress = json.loads(progress_raw.read().decode())
 
-        if progress[page] == 'complete':
+        if year:
+            progress_key = progress[year][page]
+        else:
+            progress_key = progress[page]
+
+        if progress_key == 'complete':
             logger.warning(f'Page {page} is already complete!')
             return 1
-        elif progress[page] == 'in-progress':
+        elif progress_key == 'in-progress':
             logger.warning(f'Page {page} is already in-progress!')
             return 1
         else:
@@ -353,6 +408,90 @@ class InaturalistPhotoScraper:
                                part_size=10 * 1024 * 1024)
         self.check_progress(page, mark_as_complete=True)
 
+    def _get_date(self, sort: str) -> datetime:
+        """Gets the datetime object from the current set of observations.
+
+        Args:
+            sort (str): Sort.
+
+        Returns:
+            datetime: Datetime object.
+        """
+        url = 'https://api.inaturalist.org/v2/observations'
+        resp = self._get_request(url,
+                                 params={
+                                     'taxon_id': self.taxon_id,
+                                     'order_by': 'observed_on',
+                                     'order': sort,
+                                     'fields': 'uuid,observed_on'
+                                 })
+        date_str = resp['results'][0]['observed_on']
+        date = datetime.strptime(date_str, '%Y-%m-%d')
+        return date
+
+    def _parse(self,
+               page,
+               year: Optional[int] = None) -> Optional[Union[bool, int]]:
+        """Parses the page.
+
+        Args:
+            page (int): Page number.
+            year (int, optional): Year to filter by.
+
+        Returns:
+            Returns 1 if the page is complete or in-progress, and None if
+                `mark_as_complete` is `True` or page is pending.
+        """
+        if page == self.stop_at_page:
+            logger.warning(f'Stopped at page {page} because '
+                           f'`stop_at_page` is set to {self.stop_at_page}.')
+            return 2
+
+        logger.info(f'Current page: {page}')
+
+        self.start_year = year
+        self.resume_from_page = page
+
+        if os.getenv('S3_LOGS_BUCKET_NAME'):
+            progress_status = self.check_progress(page)
+            if progress_status == 1:
+                if self.one_page_only:
+                    return 1
+                return
+        if year:
+            observations = self.get_observations(
+                page, additional_params={'year': year})
+        else:
+            observations = self.get_observations(page)
+        if not observations:
+            return
+        uuids = [x['uuid'] for x in observations['results']]
+
+        if not uuids:
+            self.data['failed_observations'].append(f'failed page: {page}')
+            if self.one_page_only:
+                return 1
+            else:
+                return
+
+        if uuids in self.data['uuids']:
+            logger.warning(f'Duplicate response in page {page}! '
+                           'Skipping...')
+            return
+        self.data['uuids'] += uuids
+        uuids = uuids[self.resume_from_uuid_index:]
+
+        for n, _uuid in enumerate(uuids, start=self.resume_from_uuid_index):
+            self.resume_from_uuid_index = n
+            logger.debug(f'Page: {page}, UUID index: {n}')
+            self.download_photos(_uuid)
+
+        if self.one_page_only:
+            return 1
+
+        if os.getenv('S3_LOGS_BUCKET_NAME'):
+            self._dump_logs(page)
+
     def run(self) -> None:
         """Runs the scraper."""
         signal.signal(signal.SIGINT, self._keyboard_interrupt_handler)
@@ -363,53 +502,47 @@ class InaturalistPhotoScraper:
         if not self.upload_to_s3:
             Path(self.output_dir).mkdir(exist_ok=True, parents=True)
 
-        num_pages = self.get_num_pages()
+        num_pages, num_observations = self.get_num_pages()
+
         pages_range = range(num_pages)
-        self.logger.info(f'Number of pages: {num_pages}')
-        self.logger.info(
-            f'Estimated number of observations: {num_pages * 200}')
+        logger.info(f'Number of pages: {num_pages}')
+        logger.info(f'Number of observations: {num_observations}')
 
-        pages_range = pages_range[self.resume_from_page:]
+        if num_observations > 10000:
+            self.is_large_results = True
+            if not self.start_year:
+                self.start_year = self._get_date('asc').year
+            if not self.end_year:
+                self.end_year = self._get_date('desc').year
 
-        for page in pages_range:
+            logger.warning(
+                'page * results_per_page > 10,000! '
+                f'Will iterate year by year starting from {self.start_year}')
 
-            if page == self.stop_at_page:
-                break
-
-            self.logger.info(f'Current page: {page}')
-
-            if os.getenv('S3_LOGS_BUCKET_NAME'):
-                progress_status = self.check_progress(page)
-                if progress_status == 1:
-                    if self.one_page_only:
-                        break
-                    continue
-
-            self.resume_from_page = page
-            uuids = self.get_observations_uuids(page)
-
-            if not uuids:
-                self.data['failed_observations'].append(f'failed page: {page}')
-                if self.one_page_only:
+            for year in range(self.start_year, self.end_year + 1):
+                if year == self.end_year:
                     break
-                else:
-                    continue
+                num_pages, num_observations = self.get_num_pages(on_year=year)
+                logger.info(f'Number of pages for {year}: {num_pages}')
+                logger.info(
+                    f'Number of observations in {year}: {num_observations}')
+                pages_range = range(num_pages)
+                pages_range = pages_range[self.resume_from_page:]
 
-            if uuids in self.data['uuids']:
-                self.logger.warning(f'Duplicate response in page {page}! '
-                                    'Skipping...')
-                continue
-            self.data['uuids'] += uuids
-            uuids = uuids[self.resume_from_uuid_index:]
+                for page in pages_range:
+                    resp_code = self._parse(page, year)
+                    if resp_code in [1, 2]:
+                        if resp_code == 1:
+                            logger.warning('Stopped because `one_page_only` '
+                                           'is set to `True`.')
+                        break
 
-            for n, _uuid in enumerate(uuids,
-                                      start=self.resume_from_uuid_index):
-                self.resume_from_uuid_index = n
-                self.logger.debug(f'Page: {page}, UUID index: {n}')
-                self.download_photos(_uuid)
-
-            if self.one_page_only:
-                break
-
-            if os.getenv('S3_LOGS_BUCKET_NAME'):
-                self._dump_logs(page)
+        else:
+            pages_range = pages_range[self.resume_from_page:]
+            for page in pages_range:
+                resp_code = self._parse(page)
+                if resp_code in [1, 2]:
+                    if resp_code == 1:
+                        logger.warning('Stopped because `one_page_only` '
+                                       'is set to `True`.')
+                    break
